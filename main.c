2a5e9c77f2e1ddafd73ba3d6cfa7d0f2633465c8
Michał <zajac-h@github.com>
2025-09-10 11:01:25 +0200

    vector2Normalize function, started working on collisions, improved comments

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #include "raylib.h"
 #include <stdlib.h>
+#include <math.h> //only for vector2Normalize function
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -32,10 +33,10 @@ int main(void)
 	struct Player
 	{
 		int score;
-		float position;
+		float positionY;
 	};
 
-	struct Ball 
+	struct Ball
 	{
 		Vector2 position;
 		Vector2 direction;
@@ -55,38 +56,50 @@ int main(void)
 
 	void resetPositions(void)
 	{
-		player1.position = SCREEN_HEIGHT/2 - PLAYER_HEIGHT/2;
+		player1.positionY = SCREEN_HEIGHT/2 - PLAYER_HEIGHT/2;
 
-		player2.position = SCREEN_HEIGHT/2 - PLAYER_HEIGHT/2;
+		player2.positionY = SCREEN_HEIGHT/2 - PLAYER_HEIGHT/2;
 
 		ball.position = (Vector2){ SCREEN_WIDTH/2, SCREEN_HEIGHT/2 };
 
+		//randomize the initial ball direction
 		switch (rand() % 2)
 		{
 		case 0:
 			ball.direction = (Vector2) { 1.0f, 0.0f };
 			break;
+
 		case 1:
 			ball.direction = (Vector2) { -1.0f, 0.0f};
 			break;
 		}
 	}
 
-	void playerClampMovement(struct Player * player) // pointer to a struct
+	// clamp vertical movement of players
+	void playerClampMovement(struct Player * player)
 	{
 		const int upperClamp = 0;
 		const int bottomClamp = SCREEN_HEIGHT - PLAYER_HEIGHT;
 
-		if (player->position <= (float)upperClamp) player->position = (float)upperClamp;
-	        if (player->position >= (float)bottomClamp) player->position = (float)bottomClamp;	
+		if (player->positionY <= (float)upperClamp)
+		{
+			player->positionY = (float)upperClamp;
+		}
+
+	    if (player->positionY >= (float)bottomClamp)
+		{
+			player->positionY = (float)bottomClamp;
+		}
 	}
 
+	// move the ball along it's direction vector
 	void moveBall()
 	{
 		ball.position.x = ball.position.x + ball.direction.x * BALL_SPEED;
 		ball.position.y = ball.position.y + ball.direction.y * BALL_SPEED;
 	}
 
+	// for detecting collisions with walls; in case of upper and lower ones also mirrors the ball direction in y axis
 	void checkWallTouch(void)
 	{
 		const int leftWall = 0 + BALL_SIZE/2;
@@ -115,17 +128,31 @@ int main(void)
 		}
 	}
 
+	Vector2 vector2Normalize(Vector2 inputVector)
+	{
+		Vector2 outputVector;
+		const float inputVectorLength = sqrt(powf(inputVector.x, 2) + powf(inputVector.y, 2));
+		outputVector = (Vector2){ inputVector.x/inputVectorLength, inputVector.y/inputVectorLength };
+		return (Vector2)outputVector;
+	}
+
 	void checkBallPlayerCollision()
 	{
 		const int boundaryLeft = 20 + BALL_SIZE/2;
 		const int boundaryRight = SCREEN_WIDTH - 20 - BALL_SIZE/2;
 
+		// TODO write a function for this
 		if (ball.position.x <= (float)boundaryLeft && 
-		    ball.position.y >= player1.position &&
-		    ball.position.y <= player1.position + PLAYER_HEIGHT)
+		    ball.position.y >= player1.positionY &&
+		    ball.position.y <= player1.positionY + PLAYER_HEIGHT)
 		{
+			const int playerCenter = player1.positionY - PLAYER_HEIGHT/2;
+			const int playerBallHeightDiff = ball.position.y - player1.positionY;
+			// ^^ results negative for upper half, positive for lower
+
 			ball.direction = (Vector2){ ball.direction.x * -1.0f,
-				          ball.direction.y * -1.0f };
+				          ball.direction.y + (float)playerBallHeightDiff/PLAYER_HEIGHT/2};
+			ball.direction = vector2Normalize(ball.direction);
 			// replace multiplying .y by -1.0f by some smart number
 			// based on how far is it from the center of player
 			//
@@ -133,8 +160,8 @@ int main(void)
 		}
 
 		if (ball.position.x >= (float)boundaryRight &&
-		    ball.position.y >= player2.position &&
-		    ball.position.y <= player2.position + PLAYER_HEIGHT)
+		    ball.position.y >= player2.positionY &&
+		    ball.position.y <= player2.positionY + PLAYER_HEIGHT)
 		{
 			ball.direction = (Vector2){ ball.direction.x * -1.0f,
 					  ball.direction.y * -1.0f };
@@ -151,12 +178,12 @@ int main(void)
 		checkBallPlayerCollision();
 
 		// player 1 movement
-		if (IsKeyDown(KEY_W)) player1.position -= PLAYER_SPEED;
-		if (IsKeyDown(KEY_S)) player1.position += PLAYER_SPEED;
+		if (IsKeyDown(KEY_W)) player1.positionY -= PLAYER_SPEED;
+		if (IsKeyDown(KEY_S)) player1.positionY += PLAYER_SPEED;
 
 		// player 2 movement
-		if (IsKeyDown(KEY_UP)) player2.position -= PLAYER_SPEED;
-		if (IsKeyDown(KEY_DOWN)) player2.position += PLAYER_SPEED;
+		if (IsKeyDown(KEY_UP)) player2.positionY -= PLAYER_SPEED;
+		if (IsKeyDown(KEY_DOWN)) player2.positionY += PLAYER_SPEED;
 
 		// player movement clamp mechanism
 		playerClampMovement(&player1);
@@ -172,12 +199,12 @@ int main(void)
 			ClearBackground(BACKGROUND_COLOR);	
 
 			// player 1
-			DrawRectangle( 10, player1.position, 
+			DrawRectangle( 10, player1.positionY, 
 				       PLAYER_WIDTH, PLAYER_HEIGHT,
 			       	       FOREGROUND_COLOR );
 
 			//player 2
-			DrawRectangle( SCREEN_WIDTH - 20, player2.position,
+			DrawRectangle( SCREEN_WIDTH - 20, player2.positionY,
 				       PLAYER_WIDTH, PLAYER_HEIGHT,
 				       FOREGROUND_COLOR );
 		
